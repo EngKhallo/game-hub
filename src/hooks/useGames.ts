9d6c6cfbb3ee1f02544e03b6d2b6fd8830d1c66f
EngKhallo <engkhalid.ict@gmail.com>
2023-05-26 22:57:57 +0300

    Fetch the genres

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,24 +1,8 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
+import { Game, FetchGamesResponse } from "../interfaces";
 
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
