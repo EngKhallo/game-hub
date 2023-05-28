307a93f9238fb729b7bbec0c0878fec104c7aef4
EngKhallo <engkhalid.ict@gmail.com>
2023-05-29 00:56:22 +0300

    Game card mobile design: responsive

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
     return (
-        <Card width='300px' borderRadius={10} overflow="hidden">
+        <Card width='400px' borderRadius={10} overflow="hidden">
             <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <HStack justifyContent='space-between' marginBottom={3}>
